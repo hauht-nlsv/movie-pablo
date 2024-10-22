f12d0e73d4e8ed4fbdce88295af9d57d6aec8a31
Hau Huynh <hauht@nhanlucsieuviet.com>
2024-10-22 15:15:29 +0700

    Commento component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Commento from "./Commento"; // Import the Commento component
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Blog</h1>
       </header>
+      <main>
+        <p>This is an example of a page where Commento.io is integrated.</p>
+
+        {/* Render the Commento component here */}
+        <Commento id="commento" />
+      </main>
     </div>
   );
 }
---
+++ src/Commento.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+
+const CONTAINER_ID = "commento";
+const SCRIPT_ID = "commento-script";
+const COMMENTO_URL = "https://cdn.commento.io/js/commento.js";
+
+interface DataAttributes {
+  [key: string]: string | boolean | undefined;
+}
+
+const insertScript = (
+  src: string,
+  id: string,
+  parentElement: HTMLElement,
+  dataAttributes: DataAttributes
+) => {
+  const script = window.document.createElement("script");
+  script.async = true;
+  script.src = src;
+  script.id = id;
+
+  Object.entries(dataAttributes).forEach(([key, value]) => {
+    if (value === undefined) {
+      return;
+    }
+    script.setAttribute(`data-${key}`, value.toString());
+  });
+
+  parentElement.appendChild(script);
+};
+
+const removeScript = (id: string, parentElement: HTMLElement) => {
+  const script = window.document.getElementById(id);
+  if (script) {
+    parentElement.removeChild(script);
+  }
+};
+
+const Commento = ({
+  id,
+  cssOverride,
+  autoInit,
+  noFonts,
+  hideDeleted,
+  pageId,
+}: {
+  id: string;
+  cssOverride?: string;
+  autoInit?: boolean;
+  noFonts?: boolean;
+  hideDeleted?: boolean;
+  pageId?: string;
+}) => {
+  useEffect(() => {
+    if (!window) {
+      return;
+    }
+    const document = window.document;
+    if (document.getElementById("commento")) {
+      insertScript(COMMENTO_URL, SCRIPT_ID, document.body, {
+        "css-override": cssOverride,
+        "auto-init": autoInit,
+        "no-fonts": noFonts,
+        "hide-deleted": hideDeleted,
+        "page-id": pageId,
+      });
+    }
+    return () => removeScript(SCRIPT_ID, document.body);
+  }, [id]);
+
+  return <div key={id} id={CONTAINER_ID} />;
+};
+export default Commento;
